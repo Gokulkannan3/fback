66950959ccb9dbf3943ae66614fba6ae8defb687
GOKUL-KANNAN-P <gokulkannanp.cse2021@citchennai.net>
2024-01-26 18:06:48 +0530

    second-commit

--- index.js
+++ index.js
@@ -1,43 +1,167 @@
-    const express = require('express');
-    const mysql = require('mysql');
-    const app = express();
-    const cors =require('cors');
-    const bcrypt = require('bcrypt');
-    const setRounds=10;
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const cors =require('cors');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const setRounds=10;
 
-    app.use(express.json());
-    app.use(cors());
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(
+    session({
+        key: "username",
+        secret: "success",
+        resave: false,
+        saveUninitialized: false,
+        cookie:{
+            expires: 60 * 10,
+        }
+    })
+)
 
     const db = mysql.createConnection({
-        user:'admin',
-        password:'Gokul2003',
-        host:'database-1.cfywi26s6zwq.us-east-2.rds.amazonaws.com',
+        user:'root',
+        password:'Gokul@003',
+        host:'localhost',
         database:'Innumvai'
     })
 
-    app.post('/order',(req,res)=>{
-        const user = req.body?.user;
-        const address = req.body?.address;
-        const coffee = req.body?.coffee;
-        const pasta = req.body?.pasta;
-        const rosemilk = req.body?.rosemilk;
-        const brownie = req.body?.brownie;
-        const cake =req.body?.cake;
-        const status = ('Booked');
-
-        console.log(req.body);
-
-        db.query('INSERT INTO orders (user,address,coffee,pasta,rosemilk,brownie,cake,status) VALUES (?,?,?,?,?,?,?,?)',[user,address,coffee,pasta,rosemilk,brownie,cake,status],
-        (err,result)=>{
-            if(err){
-                console.log(err);
-            }
-            else{
-                res.send(result);
-            }
+    const verifyJWT = (req, res, next) => {
+        const token = req.headers["x-access-token"];
+        if (!token) {
+            res.send("We need token give it next time");
+        } else {
+            jwt.verify(token, "secret", (err, decoded) => {
+                if (err) {
+                    res.json({ auth: false, message: "Failed to authenticate" });
+                } else {
+                    req.usermail = decoded.id;
+                    next();
+                }
+            });
         }
-        )
+    };
+    
+    
+    app.get('/isAuth',verifyJWT,(req,res)=>{
+        res.send("Authenticeted Successfully");
     })
+    
+    app.post('/login', async (req, res) => {
+        const username = req.body?.username;
+        const password = req.body?.password;
+    
+        db.query(
+            "SELECT * FROM signin WHERE username=?",
+            [username],
+            (err, result) => {
+                if (err) {
+                    console.log("Error:", err);
+                    res.status(500).json({ error: 'Internal Server Error' });
+                    return;
+                }
+    
+                if (result.length > 0) {
+                    bcryptjs.compare(password, result[0].password, (err, response) => {
+                        if (response) {
+                            const id  = result[0].id;
+                            const token = jwt.sign({ id }, "secret", { expiresIn: 5 });
+                            res.json({ auth: true, token: token, result: result[0], message: 'Login Successful' });
+                        } else {
+                            res.status(401).json({ message: 'Invalid Credentials' });
+                        }
+                    });
+                } else {
+                    res.status(401).json({ message: 'Invalid Credentials' });
+                }
+            }
+        );
+    });
+    
+    const verJWT = (req, res, next) => {
+        const token = req.headers["x-access-token"];
+        if (!token) {
+            res.send("We need token give it next time");
+        } else {
+            jwt.verify(token, "secret", (err, decoded) => {
+                if (err) {
+                    res.json({ auth: false, message: "Failed to authenticate" });
+                } else {
+                    req.usermail = decoded.id;
+                    next();
+                }
+            });
+        }
+    };
+    
+    
+    app.get('/isAauth', verJWT, (req, res) => {
+        const userDetails = {
+            usermail: req.usermail,
+        };
+    
+        res.json({ result: [userDetails] });
+    });
+    
+    app.post('/alogin', async (req, res) => {
+        const name = req.body?.name;
+        const password = req.body?.password;
+    
+        db.query(
+            "SELECT * FROM admin WHERE name=?",
+            [name],
+            (err, result) => {
+                if (err) {
+                    console.log("Error:", err);
+                    res.status(500).json({ error: 'Internal Server Error' });
+                    return;
+                }
+    
+                if (result.length > 0) {
+                    bcryptjs.compare(password, result[0].password, (err, response) => {
+                        if (response) {
+                            const id  = result[0].id;
+                            const token = jwt.sign({ id }, "secret", { expiresIn: 300 });
+                            res.json({ auth: true, token: token, message: 'Login Successful' });
+                        } else {
+                            res.status(401).json({ message: 'Invalid Credentials' });
+                        }
+                    });
+                } else {
+                    res.status(401).json({ message: 'Invalid Credentials' });
+                }
+            }
+        );
+    });
+
+    app.post('/order', (req, res) => {
+        const { username, address, contact, coffee, pasta, rosemilk, brownie, cake,totalAmount } = req.body;
+        const status = ('Booked');
+      
+        console.log(req.body);
+      
+        db.query(
+          'INSERT INTO orders (username, contact, address, coffee, pasta, rosemilk, brownie, cake, totalamount, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+          [username, contact, address, coffee, pasta, rosemilk, brownie, cake, totalAmount,status],
+          (err, result) => {
+            if (err) {
+              console.log(err);
+              res.status(500).send('Internal Server Error');
+            } else {
+              res.status(200).send('Order placed successfully!');
+              console.log(result)
+            }
+          }
+        );
+      });
+      
 
     app.post('/register', (req, res) => {
         const firstname = req.body?.firstname;
@@ -57,7 +181,7 @@
             return res.status(400).json({ error: 'Password and Confirm Password do not match' });
         }
 
-        bcrypt.hash(password,setRounds,(err,hash)=>{
+        bcryptjs.hash(password,setRounds,(err,hash)=>{
             if(err){
                 console.log(err)
             }
@@ -114,7 +238,7 @@
             return res.status(400).json({ error: 'Password and Confirm Password do not match' });
         }
 
-        bcrypt.hash(password,setRounds,(err,hash)=>{
+        bcryptjs.hash(password,setRounds,(err,hash)=>{
             if(err){
                 console.log(err)
             }
@@ -132,74 +256,6 @@
             }
         );
         })
-    });
-
-    app.post('/login', async (req, res) => {
-        const username = req.body?.username;
-        const password = req.body?.password;
-    
-        db.query(
-            "SELECT * FROM signin WHERE username=?",
-            [username],
-            (err, result) => {
-                if (err) {
-                    console.log("Success");
-                    res.send(result);
-                    res.status(500).json({ error: 'Internal Server Error' });
-                }
-    
-                if (result.length > 0) {
-                    bcrypt.compare(password, result[0].password, (err, response) => {
-                        if (response) {
-                            const userData = {
-                                username: result[0].username,
-                                address: result[0].address,
-                            };
-    
-                            res.status(200).json({ message: 'Login Successful', userData });
-                        } else {
-                            res.status(401).json({ message: 'Invalid Credentials' });
-                        }
-                    });
-                } else {
-                    res.status(401).json({ message: 'Invalid Credentials' });
-                }
-            }
-        );
-    });
-
-    app.post('/alogin', async (req, res) => {
-        const name = req.body?.name;
-        const password = req.body?.password;
-    
-        db.query(
-            "SELECT * FROM admin WHERE name=?",
-            [name],
-            (err, result) => {
-                if (err) {
-                    console.log("Success");
-                    res.send(result);
-                    res.status(500).json({ error: 'Internal Server Error' });
-                }
-    
-                if (result.length > 0) {
-                    bcrypt.compare(password, result[0].password, (err, response) => {
-                        if (response) {
-                            const userData = {
-                                name: result[0].name,
-                                address: result[0].address,
-                            };
-    
-                            res.status(200).json({ message: 'Login Successful', userData });
-                        } else {
-                            res.status(401).json({ message: 'Invalid Credentials' });
-                        }
-                    });
-                } else {
-                    res.status(401).json({ message: 'Invalid Credentials' });
-                }
-            }
-        );
     });
 
     app.get('/order' , (req,res) => {
